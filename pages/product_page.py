29d8fc2e57c99e9f83986e573d2b2bc73cc003e9
Сергей Дубинин <sergeidubinin1998@outlook.com>
2022-01-18 12:10:05 +0300

    Изменен тест на проверку добавления товара в корзину

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,22 +4,26 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
-        # self.should_be_button_add_to_basket()
-        self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET).click()
+        self.check_button_add_to_basket()
         self.solve_quiz_and_get_code()
         self.check_name_product()
         self.check_price_product()
 
-    def should_be_button_add_to_basket(self):
+    def check_button_add_to_basket(self):
         assert self.is_element_present(*ProductPageLocators.BUTTON_ADD_TO_BASKET), "Button 'Add to basket' is not " \
                                                                                    "presented"
+        self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET).click()
 
     def check_name_product(self):
+        assert self.is_element_present(*ProductPageLocators.NAME_PRODUCT), "Element 'Name product' is not " \
+                                                                                   "presented"
         name_product = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
         name_product_in_message = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_IN_MESSAGE).text
-        assert name_product == name_product_in_message, "Названия товаров не совпадают"
+        assert name_product == name_product_in_message, "Product names do not match"
 
     def check_price_product(self):
+        assert self.is_element_present(*ProductPageLocators.PRICE_PRODUCT), "Element 'Price product' is not " \
+                                                                           "presented"
         price_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
         price_product_in_message = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT_IN_THE_MESSAGE).text
-        assert price_product == price_product_in_message, "Цены товаров не совпадают"
+        assert price_product == price_product_in_message, "Product prices do not match"
