188bf3ded16982ae9cc863f77868ef25628a13ad
Сергей Дубинин <sergeidubinin1998@outlook.com>
2022-01-18 12:20:12 +0300

    Добавлена параметризация в тест на проверку добавления товара в корзину

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,11 @@
+import pytest
 from .pages.product_page import ProductPage
 
 
-def test_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize("value_link", ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", "offer7",
+                                        "offer8", "offer9"])
+def test_add_product_to_basket(browser, value_link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={value_link}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
